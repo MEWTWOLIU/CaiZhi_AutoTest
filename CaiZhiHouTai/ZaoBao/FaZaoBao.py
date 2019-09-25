118ac3171f59d3b3356d5b896e035040d947894f
MEWTWOLIU <475571758@163.com>
2019-09-25 14:28:07 +0800

    1.编写获取token方法 2.common文件夹增加__init__.py文件

--- CaiZhiHouTai/ZaoBao/FaZaoBao.py
+++ CaiZhiHouTai/ZaoBao/FaZaoBao.py
@@ -8,6 +8,6 @@ Peizhi = {
 brower = webdriver.Chrome()
 print(Peizhi["url"])
 brower.get(Peizhi["url"])
-# brower.page_source
- brower.implicitly_wait(10) # 10s等待
+brower.page_source
+brower.implicitly_wait(10) # 10s等待
 # brower.quit()
--- CaiZhiHouTai/common/GetToken.py
+++ CaiZhiHouTai/common/GetToken.py
@@ -1,6 +1,24 @@
-
+import requests
+from http import cookiejar
+import urllib3
 
 
 def gettoken():
+    # 测试环境
+    host = "test.qtrade.com.cn"
+    path = "/caizhi_miniapi/index/auth.do"
+    userId = "mr.joker"
+    corpId = "ww8c83d949a80b562d"
+
+    # 从cookie中获取caizhi_managekey
+    params = {"userId":userId,"corpId":corpId}
+    url = host + path
+    r = requests.get(url,params)
+    print(r)
+
+    if "caizhi_managekey" in r.cookies :
+        token = r.cookies
+
     print("token获取成功！")
-    return None
+    return token
+
