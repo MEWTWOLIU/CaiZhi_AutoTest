99ec5846d72aaea11e93a0971233a150efe28d6a
MEWTWOLIU <475571758@163.com>
2019-09-29 11:18:41 +0800

    添加注释

--- CaiZhiHouTai/common/GetCookies.py
+++ CaiZhiHouTai/common/GetCookies.py
@@ -6,8 +6,11 @@ import os
 
 
 def getCookieFromChrome():
+    # 本体存储cookie的路径
     cookiePath = os.environ['LOCALAPPDATA']+r"\Google\Chrome\User Data\Default\Cookies"
+    # 取数sqlite
     sql = "select host_key, name, encrypted_value from cookies where name = 'morning_paper_cookie'"
+    # 连接cookie文件取cookie
     with sqlite3.connect(cookiePath) as conn:
         cu = conn.cursor()
         cookies = {name:CryptUnprotectData(encrypted_value)[1].decode() for host_key,name,encrypted_value in cu.execute(sql).fetchall()}
