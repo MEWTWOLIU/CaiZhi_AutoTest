851ce239cbf403c45ed31b99c8b030e102003ce2
MEWTWOLIU <475571758@163.com>
2019-10-29 16:30:00 +0800

    新增公共类swpie

--- QiYeWeiXinAutoTest/common/swipe.py
+++ QiYeWeiXinAutoTest/common/swipe.py
@@ -4,11 +4,39 @@ from appium import webdriver
 
 def getScreenSize(driver):
 
-    driver = webdriver.Remote()
     x = driver.get_window_size()['width']
     y = driver.get_window_size()['height']
     return (x, y)
 
 
 def swipeUp(driver,t):
-    l = getScreenSize(driver)
+
+    L = getScreenSize(driver)
+    x_start = int(L[0]*0.5)
+    y_start = int(L[1]*0.75)
+    y_end = int*(L[1]*0.25)
+    driver.swipe(x_start,y_start,x_start,y_end,t)
+
+def swipeDown(driver,t):
+
+    L = getScreenSize(driver)
+    x_start = int(L[0]*0.5)
+    y_start = int(L[1]*0.25)
+    y_end = int(L[1]*0.75)
+    driver.swipe(x_start,y_start,x_start,y_end,t)
+
+def swipeRight(driver,t):
+
+    L = getScreenSize(driver)
+    x_start = int(L[0]*0.5)
+    x_end = int(L[0]*0.75)
+    y_start = int(L[1]*0.5)
+    driver.swipe(x_start,y_start,x_end,y_start,t)
+
+def swipeLeft(driver,t):
+
+    L = getScreenSize(driver)
+    x_start = int(L[0]*0.5)
+    x_end = int(L[0]*0.1)
+    y_start = int(L[0]*0.5)
+    driver.swipe(x_start,y_start,x_end,y_start,t)
